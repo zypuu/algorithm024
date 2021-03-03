f24a5a5f4bebf10a0e4f8c5348bccc4eb9efe368
zypuu <375133421@qq.com>
2021-03-03 20:18:45 +0800

    week 04

--- Week_04/canJump.go
+++ Week_04/canJump.go
@@ -1,1 +1,22 @@
-canJump.go
+// 跳跃游戏
+package main
+
+// canJump ....
+// 倒序贪心，从后开始，最先能跳到目标点的
+func canJump(nums []int) bool {
+    if len(nums) == 0 {
+        return false
+    }
+    tmp := len(nums) - 1
+    for i := len(nums) - 1; i >= 0; i-- {
+        if nums[i] + i >= tmp {
+            tmp = i
+        }
+    }
+    // 看最后是不是0
+    return tmp == 0
+}
+
+func main() {
+    
+}
